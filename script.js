45b6a6f0007fc7ef8d15673aa9720c08fcbb2f20
Mohammed KABBAJ <kabbajmohammedmk@gmail.com>
2022-12-25 17:47:06 +0100

    V1

--- script.js
+++ script.js
@@ -8,15 +8,55 @@ var fournisseurProduit = document.getElementById('fournisseurProduit');
 var dateExp = document.getElementById('dateExp');
 var pwd = document.getElementById('pwd');
 var submit = document.getElementById('submit');
+var enlig = document.getElementById('enlig');
+var bouti = document.getElementById('bouti');
+var message = document.getElementById("message");
+var categori = document.getElementsByClassName('categori');
+var cat = "";
+var listProduit = [];
+var count = 1;
+// for( let i = 0 ; i < categori.length  ; i++)
+for ( let radio of categori){
+// for (const key in categori) {
+//     const radio = categori[key];
+    try{
+        // alert(radio)
+        radio.addEventListener("change",e=>{
+            let dateRow = document.getElementById("dateRow")
+            if(e.target.value == "Nouriture"){
+                dateRow.classList.remove("hide")
+            }
+            else{
+                dateRow.classList.add("hide")
+            }
+        })
+    }
+    catch(err){
+        console.log(err)
+    }
+
+}
 
 submit.addEventListener('click', ()=>{
     // isNaN == is Not a Number
     // si isNaN = false : valuer Numerqiue
     // si isNaN = true : valuer Non Numerqiue
-    if(isNaN(valeurProduit.value)){
-        alert("Valeur produit est incorrect.");
+
+
+    if(isNaN(valeurProduit.value) || valeurProduit.value == ""){
+        //alert("Valeur produit est incorrect.");
+        messageValidation("Valeur Produit est incorrect.",false);
         return ;
     }
+
+    // Valeur du radio boutton séléctionner
+    // let varCat 
+    // for (const key in categori) {
+    //     const radio = categori[key];
+    //     if(radio.checked ){
+    //         varCat = radio.value;
+    //     }
+    // }
     var produit = {
         idProduit : idProduit.value,
         nomProduit : nomProduit.value,
@@ -24,9 +64,17 @@ submit.addEventListener('click', ()=>{
         prixProduit : prixProduit.value,
         stockProduit : stockProduit.value,
         valeurProduit : valeurProduit.value,
-        fournisseurProduit : fournisseurProduit.value
+        fournisseurProduit : fournisseurProduit.value,
+        dateExp : dateExp.value,
+        enlig : enlig.checked,
+        bouti : bouti.checked,
+        categori : cat
     }
-    console.log(produit);
+    listProduit.push(produit);
+    messageValidation("Produit bien ajouter");
+    idProduit.value = ++count;
+    // idProduit.value = listProduit.length + 1;
+    // console.log(listProduit);
 })
 
 function multiplication(v1,v2){
@@ -42,7 +90,7 @@ prixProduit.addEventListener("keyup",()=>{
         }
         valeurProduit.value = multiplication(prixProduit.value,stockProduit.value);
     } catch (error) {
-        alert(error);
+        messageValidation(error , false);
     }    
 })
 // var v = stockProduit.value
@@ -52,5 +100,17 @@ stockProduit.addEventListener("keyup",(e)=>{
     // e.target ==  stockProduit
     valeurProduit.value = multiplication(prixProduit.value,e.target.value);
 })
+
+function messageValidation(messageAfficher,valid = true){
+    message.innerHTML = messageAfficher;
+    if(valid){
+        message.classList.add("valid");
+        message.classList.remove("error");
+    }
+    else{
+        message.classList.remove("valid");
+        message.classList.add("error");
+    }
+}
 
 
